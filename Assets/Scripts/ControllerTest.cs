43b4e8942399a254c72639c69168e8050a83421b
Matthew Thompson <chatrat12@gmail.com>
2018-11-29 14:35:12 -0500

    Added OpenVR sample scene

---
+++ Assets/Scripts/ControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+
+public class ControllerTest : MonoBehaviour
+{
+    [SerializeField] private Light _light;
+
+	void Update ()
+    {
+        if (Input.GetButtonDown("Test"))
+            _light.enabled = !_light.enabled;
+		
+	}
+}
---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class GameCamera : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Testing to see if HMD is connected. Will try to make 
+        // game at least run without one.
+
+        // TODO: something :D
+        Debug.Log(XRSettings.isDeviceActive);
+    }
+
+}
