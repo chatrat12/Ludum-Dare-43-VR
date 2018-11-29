08f0b73e40426de8637064a297830ce6ecc48c8a
Matthew Thompson <chatrat12@gmail.com>
2018-11-29 16:02:16 -0500

    Added VR toggle to toolbar. Switched to .NET 4

---
+++ Assets/Scripts/VRUtil.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class VRUtil
+{
+    [MenuItem("VR/Enable")]
+    private static void EnableVR()
+    {
+        PlayerSettings.virtualRealitySupported = true;
+        Debug.Log("VR Enabled");
+    }
+
+    [MenuItem("VR/Disable")]
+    private static void DisableVR()
+    {
+        PlayerSettings.virtualRealitySupported = false;
+        Debug.Log("VR Disabled");
+    }
+}
