7bde338ba71566edfc1c7b037f11845a6c1fcff8
Matthew Thompson <chatrat12@gmail.com>
2018-12-01 15:24:14 -0500

    Failed attempt at throwing balls

---
+++ Assets/Scripts/GrippableObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SpatialTracking;
+
+public class GrippableObject : MonoBehaviour
+{
+    public Rigidbody Rigidbody { get; private set; }
+
+    protected void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //Debug.Log(_rigidbody.velocity);
+    }
+
+    public virtual void OnGripped(Transform parent, Vector3 velocity)
+    {
+        //transform.SetParent(parent);
+        //_rigidbody.isKinematic = true;
+    }
+
+    public virtual void OnReleased(Transform parent, Vector3 velocity)
+    {
+        //transform.SetParent(null);
+        //_rigidbody.isKinematic = false;
+        //_rigidbody.velocity = velocity * 100;
+        
+    }
+}
---
+++ Assets/Scripts/HandGrip.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(GripInput), typeof(TriggerVolume))]
+public class HandGrip : MonoBehaviour
+{
+    private TriggerVolume _triggerVolume;
+    private List<GrippableObject> _grippables = new List<GrippableObject>();
+    private GrippableObject _grippedObject;
+    private Vector3 _prevPosition;
+    private Vector3 _velocity;
+    private Vector3 _anchor;
+
+    private void Awake()
+    {
+        _prevPosition = transform.position;
+        _triggerVolume = GetComponent<TriggerVolume>();
+        var input = GetComponent<GripInput>();
+        input.Activated += (sender) => Grip();
+        input.Deactivated += (sender) => ReleaseGrip();
+    }
+
+    private void FixedUpdate()
+    {
+        _velocity = (transform.position - _prevPosition);//* Time.fixedDeltaTime;
+        _prevPosition = transform.position;
+        if(_grippedObject != null)
+        {
+            _grippedObject.Rigidbody.MovePosition(transform.position + _anchor);
+        }
+        //Debug.Log(_velocity);
+    }
+
+    private void Grip()
+    {
+        // TODO: Find closes to hand
+        var grippables = _triggerVolume.GetComponentsInVolume(ref _grippables);
+        var first = grippables.FirstOrDefault();
+        if (first != null)
+        {
+            _grippedObject = first;
+            _grippedObject.Rigidbody.isKinematic = false;
+            _anchor = _grippedObject.transform.position - transform.position;
+            _grippedObject.OnGripped(this.transform, _velocity);
+        }
+
+    }
+
+    private void ReleaseGrip()
+    {
+        if (_grippedObject != null)
+        {
+            _grippedObject.OnReleased(this.transform, _velocity);
+            _grippedObject = null;
+        }
+    }
+}
