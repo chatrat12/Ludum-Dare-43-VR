9940e833b31bea50dd201990548c69a77d9ea39c
Matthew Thompson <chatrat12@gmail.com>
2018-12-01 16:43:45 -0500

    Added snapping for grabbing objects.

--- Assets/Scripts/GrippableObject.cs
+++ Assets/Scripts/GrippableObject.cs
@@ -5,6 +5,9 @@ public class GrippableObject : MonoBehaviour
 {
     public Rigidbody Rigidbody { get; private set; }
 
+    [SerializeField] private float _throwVelocityModifier = 1f;
+    [SerializeField] private Transform _snappingTransfrom;
+
     protected void Awake()
     {
         Rigidbody = GetComponent<Rigidbody>();
@@ -19,13 +22,20 @@ public class GrippableObject : MonoBehaviour
     {
         transform.SetParent(parent);
         Rigidbody.isKinematic = true;
+        if(_snappingTransfrom != null)
+        {
+            var offset = _snappingTransfrom.position - transform.position;
+            transform.position = parent.position;
+            transform.position += parent.rotation * offset;
+            transform.rotation = parent.rotation * _snappingTransfrom.rotation;
+        }
     }
 
     public virtual void OnReleased(Transform parent, Vector3 velocity)
     {
         transform.SetParent(null);
         Rigidbody.isKinematic = false;
-        Rigidbody.velocity = velocity;
+        Rigidbody.velocity = velocity * _throwVelocityModifier;
         
     }
 }
