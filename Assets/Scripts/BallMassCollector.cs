b7749818e2d72e0e8e627a0b6b1244356cf3488c
Matthew Thompson <chatrat12@gmail.com>
2018-12-02 13:13:38 -0500

    Fixed physics layers. Things to be collected by the ball should be on the BallMass layer.

---
+++ Assets/Scripts/BallMassCollector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BallMassCollector : MonoBehaviour
+{
+    private GameBall _gameBall;
+
+    private void Awake()
+    {
+        _gameBall = GetComponentInParent<GameBall>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        var mass = other.GetComponent<BallMass>();
+        if (mass != null && !mass.Attached)
+        {
+            _gameBall.AttachMass(mass);
+        }
+    }
+}
