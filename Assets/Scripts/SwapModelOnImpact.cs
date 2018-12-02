956b145040d2391b6962e6bf538bb49a816f195a
Ian <me@ianstrandberg.com>
2018-12-01 19:21:28 -0800

    destructble building progress

---
+++ Assets/Scripts/SwapModelOnImpact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapModelOnImpact : MonoBehaviour {
+    public GameObject DestructibleModel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+
+        Instantiate(DestructibleModel, transform);
+    }
+
+
+}
