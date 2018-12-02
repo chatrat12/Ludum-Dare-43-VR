690ee2d438f4e00543408238acd25bacd40190c6
Ian <me@ianstrandberg.com>
2018-12-01 22:15:55 -0800

    model swap for destructible buildings

--- Assets/Scripts/SwapModelOnImpact.cs
+++ Assets/Scripts/SwapModelOnImpact.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwapModelOnImpact : MonoBehaviour {
+    public GameObject WholeModel;
     public GameObject DestructibleModel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnTriggerEnter(new Collider());
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
 
-        Instantiate(DestructibleModel, transform);
+        WholeModel.GetComponent<MeshRenderer>().enabled = false;
+        
+        var destructible = Instantiate(DestructibleModel);
+        destructible.transform.position = WholeModel.transform.position;
+        //destructible.transform.rotation = WholeModel.transform.rotation;
+        //destructible.transform.localScale = WholeModel.transform.localScale;
     }
 
 
