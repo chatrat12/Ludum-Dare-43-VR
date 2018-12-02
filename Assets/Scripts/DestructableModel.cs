5a3a26d991fbae740774e6739370342d05c6f7d2
Matthew Thompson <chatrat12@gmail.com>
2018-12-02 14:19:25 -0500

    Exposed debri impact force modifier.

--- Assets/Scripts/DestructableModel.cs
+++ Assets/Scripts/DestructableModel.cs
@@ -5,11 +5,11 @@ public class DestructableModel : MonoBehaviour
 {
     [SerializeField] private GameObject _fracturedModel;
     [SerializeField] private float _breakForce = 100;
+    [SerializeField] private float _debrisImpactForceModifier = 1f;
 
     private void OnCollisionEnter(Collision collision)
     {
         var mass = collision.collider.GetComponentsInChildren<Rigidbody>().Sum(rb => rb.mass);
-
         if (collision.relativeVelocity.magnitude * mass > _breakForce)
         {
             // Disable previous collision so we can spawn in
@@ -21,7 +21,7 @@ public class DestructableModel : MonoBehaviour
             // Apply impact force to debris
             var rigidbodies = fracturedModel.GetComponentsInChildren<Rigidbody>();
             foreach (var rb in rigidbodies)
-                rb.AddForce(collision.relativeVelocity * mass, ForceMode.Force);
+                rb.AddForce(collision.relativeVelocity * mass * _debrisImpactForceModifier, ForceMode.Force);
 
         }
     }
