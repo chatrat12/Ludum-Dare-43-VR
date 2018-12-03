7c81ef3dba6120025fe069cc0db79bfba1a48b6e
Matthew Thompson <chatrat12@gmail.com>
2018-12-03 13:48:24 -0500

    Updated pause script to only pause flying balls.

--- Assets/Scripts/DebugGamePause.cs
+++ Assets/Scripts/DebugGamePause.cs
@@ -6,7 +6,22 @@ public class DebugGamePause : MonoBehaviour
     private void Update()
     {
         if (Input.GetButton("DebugPause"))
-            UnityEditor.EditorApplication.isPaused = true;
+        {
+            var balls = FindObjectsOfType<GameBall>();
+            foreach (var ball in balls)
+            {
+                if(!ball.Rigidbody.isKinematic)
+                {
+                    foreach (var rb in ball.GetComponentsInChildren<Rigidbody>())
+                        Destroy(rb);
+                    foreach (var collider in ball.GetComponentsInChildren<Collider>())
+                        Destroy(collider);
+
+                    Destroy(ball);
+                    ball.GetComponentInChildren<TrailRenderer>().time = float.MaxValue;
+                }
+            }
+        }
     }
 }
 #endif
