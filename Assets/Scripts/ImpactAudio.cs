417619c6246492c4f55f1fc5e1bd61d4aca1d453
Matthew Thompson <chatrat12@gmail.com>
2018-12-03 19:33:10 -0500

    Added impact sounds, ball times out

---
+++ Assets/Scripts/ImpactAudio.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ImpactAudio : MonoBehaviour
+{
+    [SerializeField] private float _threshold;
+    [SerializeField] private AudioClip[] _clips;
+    private AudioSource _source;
+
+    private void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > _threshold)
+            PlayRandomClip();
+    }
+
+    private void PlayRandomClip()
+    {
+        if (_clips.Length > 0)
+        {
+            _source.clip = _clips[Random.Range(0, _clips.Length)];
+            _source.Play();
+        }
+    }
+}
